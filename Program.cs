a826b0ceac553a0b22d0333e63f9d98196e6f0cf
vpradhap <pradhapvenkat@gmail.com>
2022-05-29 17:04:19 +0530

    Added : Validating first name

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿
+﻿using UserRegistration;
+
 Console.WriteLine("\n\t\tUser Registration Program");
+UserValidation validation = new UserValidation(); 
+int flag = 0;
+while (flag == 0)
+{
+    Console.WriteLine("\n1 - Validate first name");
+
+    Console.Write("\nEnter your choice : ");
+    string pick = Console.ReadLine();
+    switch (pick)
+    {   
+        case "1":
+            validation.FirstName();
+            break;
+        default:
+            flag = 1;
+            Console.WriteLine("\nEnter valid choice");
+            break;
+    }
+}
---
+++ UserValidation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class UserValidation
+    {
+        public void FirstName()
+        {
+            string CONDITION = "^[A-Z][A-Za-z]{2,}$";
+
+            Console.Write("\nEnter your first name : ");
+            string input = Console.ReadLine();
+
+            if(Regex.IsMatch(input, CONDITION))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n"+input + " is valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n"+input + " is invalid\t ( Try again )");
+                Console.ResetColor();
+            }
+        }
+    }
+}
